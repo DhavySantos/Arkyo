1c3a1796df54511ceb95f5cf6da4a8407539a168
DhavyLTS <dhavy.s@proton.me>
2024-02-25 10:20:18 -0300

    reafactor: remove params field.

--- src/modules/route.rs
+++ src/modules/route.rs
@@ -1,53 +1,17 @@
-use std::str::FromStr;
-
-use super::{request::Request, response::Response};
+use super::prelude::*;
 
 #[derive(Debug, Clone)]
 pub struct Route {
-    pub method: Method,
-    pub path: Vec<String>,
-    pub params: Vec<String>,
     pub handler: fn(Request) -> Response,
-}
-
-#[derive(PartialEq, Debug, Clone)]
-pub enum Method {
-    Delete,
-    Post,
-    Get,
-    Put,
-}
-
-impl FromStr for Method {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "DELETE" => Ok(Method::Delete),
-            "POST" => Ok(Method::Post),
-            "GET" => Ok(Method::Get),
-            "PUT" => Ok(Method::Put),
-            _ => Err(()),
-        }
-    }
+    pub path: Vec<String>,
+    pub method: Method,
 }
 
 impl Route {
     pub fn new(path: String, method: Method, handler: fn(Request) -> Response) -> Route {
-        let parts: Vec<&str> = path.split("/").skip(1).collect();
-        let mut params: Vec<String> = Vec::new();
-        let mut path: Vec<String> = Vec::new();
+        let mut path: Vec<String> = path.split("/").map(|s| s.to_string()).collect();
+        path.retain(|x| !x.is_empty());
         
-        for char in parts {
-            if char.starts_with(":") {
-                let param = char.trim_start_matches(":");
-                let param  = param.to_string();
-                path.push(String::from("*"));
-                params.push(param);
-            } else {
-                path.push(char.to_string());
-            }
-        }
-
-        Route { path, method, handler, params }
+        Route { path, method, handler }
     }
 }
