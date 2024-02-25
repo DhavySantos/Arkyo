e959beb6893fb2647406e85c31495667abea939a
DhavyLTS <dhavy.s@proton.me>
2024-02-25 10:21:24 -0300

    refactor: new parse logic

--- src/modules/request.rs
+++ src/modules/request.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, io::Error, str::FromStr};
 
-use super::route::Method;
+use crate::modules::http::Method;
 
 #[derive(Debug)]
-pub struct Request {
+pub struct Request { 
     pub headers: HashMap<String, String>,
     pub params: HashMap<String, String>,
     pub path: Vec<String>,
@@ -12,31 +12,38 @@ pub struct Request {
 }
 
 impl Request {
-    pub fn new(mut request: &str) -> Request {
-        let body = request.split("\r\n\r\n").nth(1).unwrap();
-        request = request.split("\r\n\r\n").nth(0).unwrap();
-
-        let method = request.split(" ").nth(0).unwrap();
-        let path = request.split(" ").nth(1).unwrap();
-
-        let lines: Vec<&str> = request.split("\r\n").skip(1).collect();
+    pub fn new(request: &str) -> Result<Self, Error> {
         
-        let mut headers = HashMap::new();
+        let protocol = request.split(" ").nth(2).unwrap().to_string();
+        let method = request.split(" ").nth(0).unwrap().to_string();
+        let path = request.split(" ").nth(1).unwrap().to_string();
+        
+        let mut headers: HashMap<String, String> = HashMap::new();
+        let params = HashMap::new();
 
+        let mut path: Vec<String> = path.split("/").map(|s| s.to_string()).collect();
+        path.retain(|x| !x.is_empty());
+        
+        let method = match Method::from_str(&method) {
+            Err(_) => return Err(Error::new(std::io::ErrorKind::Other, "Method not supported")),
+            Ok(method) => method,
+        };
+
+        if !protocol.starts_with("HTTP") {
+            return Err(Error::new(std::io::ErrorKind::Other, "Protocol not supported"));
+        }
+        
+        let lines: Vec<&str> = request.lines().skip(1).collect();
+        
         for line in lines {
-            let mut parts = line.split(": ");
-            let key = parts.next().unwrap();
-            let value = parts.next().unwrap();
-            headers.insert(key.to_string(), value.to_string());
-        }
-        
-        Request {
-            path: path.split("/").skip(1).map(|s| s.to_string()).collect(),
-            method: Method::from_str(method).unwrap(),
-            body: body.to_string(),
-            params: HashMap::new(),
-            headers,
-        }
+            if line.is_empty() { break; }
+            let mut parts = line.split_whitespace();
+            let key = parts.next().unwrap().to_string();
+            let value = parts.next().unwrap().to_string();
+            headers.insert(key, value);
+        };
 
+        let body = request.split("\r\n\r\n").nth(1).unwrap().to_string();
+        Ok(Self { headers, body, params, path, method  })
     }
 }
