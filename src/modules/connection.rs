202bb9344286ab1057bbb3276ddf93d77b76eaa9
DhavyLTS <dhavy.s@proton.me>
2024-03-01 22:48:00 -0300

    feat: simple middleware handler

--- src/modules/connection.rs
+++ src/modules/connection.rs
@@ -3,26 +3,27 @@ use std::net::TcpStream;
 use std::path::Path;
 use std::fs::File;
 
+use super::middleware::Middleware;
 use super::route::Route;
 use super::prelude::*;
 
-pub fn handler(mut stream: TcpStream, routes: Vec<Route>, static_folder: Option<String>) {
+pub fn handler(mut stream: TcpStream, middlewares: Vec<Middleware>, routes: Vec<Route>, static_folder: Option<String>) {
     let mut buf = [0; 1024];
 
     let request_str = match stream.read(&mut buf) {
-        Ok(size) => String::from_utf8_lossy(&buf[..size]),
         Err(_) => {
             let response = Response::new().status(Status::InternalServerError);
             return stream.write_all(response.to_string().as_bytes()).unwrap()
         },
+        Ok(size) => String::from_utf8_lossy(&buf[..size]),
     };
 
     let mut request = match Request::new(&request_str) {
-        Ok(request) => request,
         Err(_) => {
             let response = Response::new().status(Status::BadRequest);
             return stream.write_all(response.to_string().as_bytes()).unwrap()
-        }
+        }, 
+        Ok(request) => request,
     };
 
     if let Some(static_folder) = &static_folder {
@@ -47,45 +48,46 @@ pub fn handler(mut stream: TcpStream, routes: Vec<Route>, static_folder: Option<
             return stream.write_all(response.to_string().as_bytes()).unwrap();
         }
     }
-    
+
     let mut opt_route: Option<&Route> = None;
 
     for route in routes.iter() {
-        if route.path.len() != request.path.len() || route.method != request.method { continue;}
-        
-        let mut path: Vec<String> = Vec::new();
-        
-        for (request_path, route_path) in request.path.iter().zip(route.path.iter()) {
-                //println!("{} {}", request_path, route_path);
-                if !route_path.starts_with(":") && request_path != route_path { break; }
-                
-                if route_path.starts_with(":") {
-                    let key = route_path[1..].to_string();
-                    let value = request_path.to_string();
-                    path.push(route_path.to_string());
-                    request.params.insert(key, value);
-                    continue;
-                } 
+        if route.path.len() != request.path.len() || route.method != request.method { continue; }
 
-                if route_path == request_path { 
-                    path.push(route_path.to_string());
-                    continue; 
-                }
+        let mut path: Vec<String> = Vec::new();
+
+        for (request_path, route_path) in request.path.iter().zip(route.path.iter()) {
+            if !route_path.starts_with(":") && request_path != route_path { break; }
+
+            if route_path.starts_with(":") {
+                let key = route_path[1..].to_string();
+                let value = request_path.to_string();
+                request.params.insert(key, value);
+                path.push(route_path.to_string());
+                continue;
+            } 
+
+            path.push(route_path.to_string());
         }
-        
+
         if path == route.path { 
             opt_route = Some(route); 
             break; 
         }
 
     }
-    
-    let response = match opt_route {
-        Some(route) => (route.handler)(request),
-        None => {
-            let response = Response::new().status(Status::NotFound);
-            return stream.write_all(response.to_string().as_bytes()).unwrap()
-        },
-    };
-    stream.write_all(response.to_string().as_bytes()).unwrap();
+
+    let mut response: Option<Response> = None;
+
+    if let Some(route) = opt_route {
+        for middleware in middlewares {
+            match middleware.handle(request.clone()) {
+                Err(_response) => { response = Some(_response); break; }
+                Ok(_request) => { request = _request; },
+            }
+        }
+        if response.is_none() { response = Some(route.handle(request)); }
+    } else { response = Some(Response::new().status(Status::NotFound)); }
+
+    stream.write_all(response.unwrap().to_string().as_bytes()).unwrap();
 }
