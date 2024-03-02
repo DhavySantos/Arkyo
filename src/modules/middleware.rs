e96364cfb9e6a0b8a973d858d155f9c0486ed56c
DhavyLTS <dhavy.s@proton.me>
2024-03-01 23:54:25 -0300

    feat: add path field to middleware struct.

--- src/modules/middleware.rs
+++ src/modules/middleware.rs
@@ -4,12 +4,15 @@ use super::request::Request;
 #[derive(Debug, Clone)]
 pub struct Middleware {
     handler: fn(Request) -> Result<Request,Response>,
+    pub path: Vec<String>
 }
 
 impl Middleware {
 
-    pub fn new( handler: fn(Request) -> Result<Request, Response>) -> Self {
-        Self { handler }
+    pub fn new( path: String, handler: fn(Request) -> Result<Request, Response>) -> Self {
+        let mut path: Vec<String> = path.split("/").map(|s| s.to_string()).collect();
+        path.retain(|s| !s.is_empty());
+        Self { handler, path }
     } 
 
     pub fn handle(&self, request: Request) -> Result<Request, Response> {
