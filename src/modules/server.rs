a91978d0cda195af1f5015ea6c5618d40d920b1b
DhavyLTS <dhavy.s@proton.me>
2024-03-01 22:47:37 -0300

    feat: add middleware support.

--- src/modules/server.rs
+++ src/modules/server.rs
@@ -1,22 +1,29 @@
 use std::net::TcpListener;
 
+
+use super::middleware::Middleware;
 use super::connection::handler;
 use super::route::Route;
 use super::prelude::*;
 
 pub struct Server { 
     static_folder: Option<String>,
+    middleware: Vec<Middleware>, 
     routes: Vec<Route>,
 }
 
 impl Server {
 
     pub fn new() -> Server {
-        Server { routes: Vec::new(), static_folder: None }
+        Server { routes: Vec::new(), middleware: Vec::new(), static_folder: None }
     }
 
     pub fn add_route(&mut self, path: &str, method: Method , handler: fn(Request) -> Response) {
         self.routes.push(Route::new(path.to_string(), method, handler));
+    }
+
+    pub fn add_middleware(&mut self, handler: fn(Request) -> Result<Request, Response>) {
+        self.middleware.push(Middleware::new(handler))
     }
 
     pub fn static_folder(&mut self, path: &str) {
@@ -33,8 +40,9 @@ impl Server {
             match stream {
                 Ok(stream) =>  {
                     let routes = self.routes.clone();
+                    let middlewares = self.middleware.clone();
                     let static_folder = self.static_folder.clone();
-                    std::thread::spawn(move || handler(stream, routes, static_folder));
+                    std::thread::spawn(move || handler(stream, middlewares, routes, static_folder));
                 },
                 Err(err) => return println!("Connection failed: {:?}", err), 
             }   
