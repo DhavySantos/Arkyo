b85b0e514142b1ab63d56dcb4702c9591c3b3b6e
DhavyLTS <dhavy.s@proton.me>
2024-03-01 22:45:41 -0300

    feat: add mod middleware;

--- src/modules.rs
+++ src/modules.rs
@@ -1,10 +1,10 @@
 mod connection;
+pub mod middleware;
 pub mod response;
 pub mod request;
 pub mod server;
 pub mod route;
 pub mod http;
-
 
 pub mod prelude {
     pub use super::response::Response;
