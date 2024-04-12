93ffd90a979b664a6166cc810b6bd066edd5a8bf
DhavyLTS <dhavy.s@proton.me>
2024-04-12 00:26:50 +0000

    feat: impl regex on route
    impl regex field on route struct
    create a regex using path.

--- src/core/_route.rs
+++ src/core/_route.rs
@@ -1,23 +1,29 @@
 use crate::network::{Method, Request, Response};
+use regex::Regex;
 
 #[derive(Clone)]
 pub struct Route { 
     handler: fn(&mut Request, &mut Response),
     method: Method,
+    regex: Regex,
     path: String,
 }
 
 impl Route {
     pub fn new(path: String, method: Method, handler: fn(&mut Request, &mut Response)) -> Route {
-        Route { path, method, handler}    
+        let regex = Regex::new(r"(:\w+)").unwrap();
+        let regex = regex.replace_all(&path, r"([^/]+)");
+        let regex = Regex::new(&regex).unwrap();
+        
+        Route { path, method, handler, regex }    
     }
     
     pub fn handle(&self, request: &mut Request, response: &mut Response) {
         (self.handler)(request, response);
     }
 
-    pub fn path(&self) -> String {
-        self.path
+    pub fn path(&self) -> &String {
+        &self.path
     }
 
     pub fn method(&self) -> &Method { 
