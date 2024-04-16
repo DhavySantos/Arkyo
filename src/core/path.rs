178d612648e3450a48a2ab4bdcc887747ee8d583
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-04-16 11:00:59 -0300

    Example of test implementation
    cfg_attr shall be used to derive the debug required implementations when
    testing.

--- src/core/path.rs
+++ src/core/path.rs
@@ -1,36 +1,61 @@
 use regex::Regex;
 
-pub enum PathErrors {
-
-}
+#[cfg_attr(test, derive(Debug, PartialEq))]
+pub enum PathErrors {}
 
 #[derive(Clone)]
+#[cfg_attr(test, derive(Debug))]
 pub struct Path {
-	value: String,
-	regex: Regex,
+    value: String,
+    regex: Regex,
 }
 
 impl Path {
-	pub fn parse(value: String) -> Result<Self, PathErrors> {
-		let regex = Regex::new(r"(:\w+)").unwrap();
+    pub fn parse(value: String) -> Result<Self, PathErrors> {
+        let regex = Regex::new(r"(:\w+)").unwrap();
         let regex = regex.replace_all(&value, r"([^/]+)") + "/?$";
         let regex = Regex::new(&regex).unwrap();
 
-		Ok(Self {
-			value,
-			regex,
-		})
-	}
+        Ok(Self { value, regex })
+    }
 
-	pub fn as_str(&self) -> &str {
-		self.value.as_str()
-	}
+    pub fn as_str(&self) -> &str {
+        self.value.as_str()
+    }
 
-	pub fn as_regex(&self) -> &Regex {
-		&self.regex
-	}
+    pub fn as_regex(&self) -> &Regex {
+        &self.regex
+    }
 
-	pub fn is_match(&self, input: &str) -> bool {
-		self.regex.is_match(input)
-	}
+    pub fn is_match(&self, input: &str) -> bool {
+        self.regex.is_match(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fmt::Formatter;
+
+    impl PartialEq for Path {
+        fn eq(&self, other: &Self) -> bool {
+            self.value == other.value
+        }
+    }
+
+    #[test]
+    fn parse_path() {
+        let cases = vec![(
+            "aaa",
+            Ok(Path {
+                value: String::from("aaa"),
+                regex: Regex::new("aaa").unwrap(),
+            }),
+        )];
+
+        for (path_str, correct) in cases {
+            let path = Path::parse(path_str.to_string());
+            assert_eq!(path, correct);
+        }
+    }
 }
