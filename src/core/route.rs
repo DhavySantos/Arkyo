e8c74574b3b7094706c82f48d3ebb6f11af8de5a
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-04-16 14:08:04 -0300

    Defined base for tests on route.rs

--- src/core/route.rs
+++ src/core/route.rs
@@ -1,7 +1,8 @@
-use crate::network::{Method, Request, Response};
 use super::Path;
+use crate::network::{Method, Request, Response};
 
 #[derive(Clone)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct Route {
     handler: fn(Request) -> Response,
     method: Method,
@@ -10,20 +11,31 @@ pub struct Route {
 
 impl Route {
     pub fn new(path: Path, method: Method, handler: fn(Request) -> Response) -> Route {
-        Route { path, method, handler }
+        Route {
+            path,
+            method,
+            handler,
+        }
     }
 
-    pub fn compare(&self, input: &str) -> bool { self.path.is_match(input) }
+    pub fn compare(&self, input: &str) -> bool {
+        self.path.is_match(input)
+    }
 
     pub fn handle(&self, request: Request) -> Response {
         (self.handler)(request)
     }
 
     pub fn path(&self) -> &str {
-        &self.path.as_str()
+        self.path.as_str()
     }
 
     pub fn method(&self) -> &Method {
         &self.method
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
