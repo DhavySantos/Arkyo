c5e89a7da98ff754daaeae269f1c1d7108586cb5
DhavyLTS <dhavy.s@proton.me>
2024-04-20 17:09:04 -0300

    fix: change export method

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,11 +1,13 @@
 mod middleware;
 mod pipeline;
-mod server;
 mod route;
-mod path;
+
+pub mod server;
+pub mod path;
 
 pub use self::middleware::*;
 pub use self::pipeline::*;
-pub use self::server::*;
 pub use self::route::*;
-pub use self::path::*;
+
+pub use self::server::Server;
+pub use self::path::Path;
