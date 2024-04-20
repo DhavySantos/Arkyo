058ec63773ee59fba34e881117a09739dbb6bc00
DhavyLTS <dhavy.s@proton.me>
2024-04-20 17:09:25 -0300

    feat: impl prelude mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 pub mod core;
 pub mod network;
+
+pub mod prelude { 
+    pub use crate::core::{
+        Middleware,
+        Server,
+        Route,
+    };
+    
+    pub use crate::network::{
+        Response,
+        Request,
+        Method,
+        Status,
+    };
+}
