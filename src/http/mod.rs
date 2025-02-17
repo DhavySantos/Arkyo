a98121f9aa5901a98759120def74b4c9e09b48b1
Dhavy Santos <dhavysantos@gmail.com>
2025-02-17 20:58:32 -0300

    init: http module

---
+++ src/http/mod.rs
@@ -0,0 +1,11 @@
+mod method;
+mod request;
+mod response;
+mod server;
+mod status;
+
+pub use method::Method;
+pub use request::Request;
+pub use response::Response;
+pub use server::Server;
+pub use status::Status;
