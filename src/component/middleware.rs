262d2a730edad34908486ad1b013a284e12f4944
Dhavy Santos <dhavysantos@gmail.com>
2025-02-18 00:43:59 -0300

    refactor: middleware handler to use Arc callback
    - Replaced `RouteHandler` function pointer with an `Arc<dyn Fn>` for
    flexibility.
    
    - Updated `Middleware` struct to store the callback in an `Arc`.-
    Adjusted `Route::new` to accept any function matching the signature and
    store it in `Arc`.
    
    - Updated `handle` method to invoke the callback properly.- Removed
    `RouteHandler` type alias as it is no longer needed.
    
    - Adjusted module exports to reflect these changes.

--- src/component/middleware.rs
+++ src/component/middleware.rs
@@ -1,25 +1,34 @@
 use crate::http::{Request, Response};
 use crate::util::Location;
+use std::sync::Arc;
 
-pub type MiddlewareHandler = fn(&mut Request, &mut Response);
-
-#[cfg_attr(debug_assertions, derive(Debug))]
 #[derive(Clone)]
 pub struct Middleware {
-    handler: MiddlewareHandler,
+    callback: Arc<dyn Fn(&mut Request, &mut Response)>,
     location: Location,
 }
 
 impl Middleware {
-    pub fn new(location: Location, handler: MiddlewareHandler) -> Self {
-        Self { location, handler }
+    pub fn new<T: Fn(&mut Request, &mut Response) + 'static>(
+        location: Location,
+        callback: T,
+    ) -> Self {
+        let callback = Arc::new(callback);
+        Self { location, callback }
     }
 
     pub fn handle(&self, req: &mut Request, res: &mut Response) {
-        (self.handler)(req, res)
+        (self.callback)(req, res)
     }
 
     pub fn is_match(&self, location: &str) -> bool {
-        self.location.start_with(location)
+        self.location.is_match(location)
+    }
+}
+
+#[cfg(debug_assertions)]
+impl std::fmt::Debug for Middleware {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "")
     }
 }
