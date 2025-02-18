41eb9ceb53dae9c511442396b9f5b6a761d655ab
Dhavy Santos <dhavysantos@gmail.com>
2025-02-18 00:45:34 -0300

    refactor: Route struct to use Arc for callback handler
    - Replaced the RouteHandler type alias with a callback field of type
    Arc<dyn Fn(&mut Request, &mut Response)>, allowing dynamic dispatch.
    
    - Updated the Route constructor to accept a generic function callback.
    
    - Modified the handle method to call the callback via Arc.
    
    - Improved flexibility and memory safety by using Arc for shared
    ownership of the callback.

--- src/component/route.rs
+++ src/component/route.rs
@@ -1,29 +1,31 @@
 use crate::http::{Method, Request, Response};
 use crate::util::Location;
-
 use std::collections::HashMap;
+use std::sync::Arc;
 
-pub type RouteHandler = fn(&mut Request, &mut Response);
-
-#[cfg_attr(debug_assertions, derive(Debug))]
 #[derive(Clone)]
 pub struct Route {
-    handler: RouteHandler,
+    callback: Arc<dyn Fn(&mut Request, &mut Response)>,
     location: Location,
     method: Method,
 }
 
 impl Route {
-    pub fn new(location: Location, method: Method, handler: RouteHandler) -> Self {
+    pub fn new<T: Fn(&mut Request, &mut Response) + 'static>(
+        location: Location,
+        method: Method,
+        callback: T,
+    ) -> Self {
+        let callback = Arc::new(callback);
         Self {
             location,
-            handler,
+            callback,
             method,
         }
     }
 
     pub fn handle(&self, req: &mut Request, res: &mut Response) {
-        (self.handler)(req, res);
+        (self.callback)(req, res);
     }
 
     pub fn method(&self) -> &Method {
@@ -32,5 +34,12 @@ impl Route {
 
     pub fn is_match(&self, location: &str) -> bool {
         self.location.start_with(location)
+    }
+}
+
+#[cfg(debug_assertions)]
+impl std::fmt::Debug for Route {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "")
     }
 }
